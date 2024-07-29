2ac346d0077d96206b45320bb6a1e7ed48bd6410
Nate McKenzie <nate6264@gmail.com>
2024-07-29 11:01:48 -0600

    RegisterBank implementation

--- src/main/kotlin/cpu/Executive.kt
+++ src/main/kotlin/cpu/Executive.kt
@@ -7,7 +7,6 @@ import shortToBytes
 import java.util.Timer
 import kotlin.concurrent.timer
 
-@OptIn(ExperimentalUnsignedTypes::class)
 class Executive (
     parserReference: Parser
 ){
@@ -35,9 +34,9 @@ class Executive (
            memory.read(shortToBytes(pc)),
             memory.read(shortToBytes((pc+1u).toUShort())),
         )
-
         //Restore memory driver state
         if(switched) memory.switch()
+
         val nibbleds = Pair(nibble(line.first),nibble(line.second))
         val instruction = parser.parse(nibbleds)
         val newPC = instruction.run(nibbleds, shortToBytes(pc))
--- src/main/kotlin/cpu/RegisterBank.kt
+++ src/main/kotlin/cpu/RegisterBank.kt
@@ -1,15 +1,16 @@
 package cpu
 
+@OptIn(ExperimentalUnsignedTypes::class)
 class RegisterBank {
     private val general = UByteArray(8) {0u}
     val address = UByteArray(2) {0u}
-    val timer = 0u
+    var timer = 0u
 
-    fun writeRegister(address: Byte, value: Byte){
-        TODO("Not implemented yet")
+    fun writeRegister(address: UByte, value: UByte){
+        general[address.toInt()] = value
     }
 
-    fun readRegister(address: Byte) : Byte{
-        TODO("Not implemented yet")
+    fun readRegister(address: UByte) : UByte{
+        return general[address.toInt()]
     }
 }
